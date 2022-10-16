2c003dae4703c661b604d062f3d04f7ef64ddde4
Oluwapelumi Odueso <oduesopelumi2@gamil.com>
2022-10-16 11:28:51 +0100

    Learning Go

--- Month2/Week5/Cli-todo/main.go
+++ Month2/Week5/Cli-todo/main.go
@@ -6,11 +6,73 @@ import (
 )
 
 var TODOLIST = []string{"Default Todo"}
+var menuNumber int
 
 func main() {
 	welcome()
-	displayMenu()
-	var menuNumber int
+	menu()
+}
+
+func newLine(numberOfLines int) {
+	i := 0
+	for i < numberOfLines {
+		fmt.Print("\n")
+		i++
+	}
+}
+
+func welcome() {
+	fmt.Println("********{Welcome to the TODO CLI app}*******")
+	newLine(1)
+}
+
+func listTodo() {
+	fmt.Println("List Of Items in Your Todo list are:")
+	newLine(
+		1)
+	for index, item := range TODOLIST {
+		fmt.Printf("TODO #%v: %v", index+1, item)
+	}
+	newLine(2)
+	menu()
+}
+
+/*func displayMenu() {
+	fmt.Println("Select Operation")
+	fmt.Println("1. Create Todo \t\t 2. List Todo items")
+	fmt.Println("3. Edit Todo items\t 4. Delete Todo items")
+	fmt.Println("0. Exit Program \t")
+	newLine(2)
+}*/
+
+func createTodo() {
+	fmt.Println("Creating Todo Items")
+	menu()
+}
+
+func editTodo() {
+	fmt.Println("Editing todo item")
+	menu()
+}
+
+func deleteTodo() {
+	fmt.Println("Deleting todo item")
+	menu()
+}
+
+func exitProgram() {
+	fmt.Println("Goodbye.")
+	os.Exit(0)
+}
+
+func menu() {
+	newLine(1)
+	fmt.Println("Select Operation")
+	fmt.Println("1. Create Todo \t\t 2. List Todo items")
+	fmt.Println("3. Edit Todo items\t 4. Delete Todo items")
+	fmt.Println("0. Exit Program ")
+	newLine(2)
+
 	_, err := fmt.Scan(&menuNumber)
 	if err != nil {
 		fmt.Println("Error: please select the correct menu Item.")
@@ -31,54 +93,4 @@ func main() {
 	default:
 		fmt.Println("Error: Invalid Input")
 	}
-}
-
-func newLine(numberOfLines int) {
-	i := 0
-	for i < numberOfLines {
-		fmt.Print("\n")
-		i++
-	}
-}
-
-func welcome() {
-	fmt.Println("********{Welcome to the TODO CLI app}*******")
-	newLine(3)
-}
-
-func listTodo() {
-	fmt.Println("List Of Items in Your Todo list are:")
-	newLine(
-		1)
-	for index, item := range TODOLIST {
-		fmt.Printf("TODO #%v: %v", index+1, item)
-	}
-	newLine(2)
-}
-
-func displayMenu() {
-	fmt.Println("Select Operation")
-	fmt.Println("1. Create Todo \t")
-	fmt.Println("2. List Todo \t")
-	fmt.Println("3. Edit Todo \t")
-	fmt.Println("4. Delete Todo \t")
-	fmt.Println("0. Exit Program \t")
-	newLine(2)
-}
-
-func createTodo() {
-	fmt.Println("Creating Todo Items")
-}
-
-func editTodo() {
-	fmt.Println("Editing todo item")
-}
-
-func deleteTodo() {
-	fmt.Println("Deleting todo item")
-}
-
-func exitProgram() {
-	fmt.Println("Goodbye.")
-	os.Exit(0)
 }
